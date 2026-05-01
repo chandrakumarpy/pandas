221ce73a91eaba46e4c63264b0ddac05ec051c33
chandu <94897352+chandrakumarpy@users.noreply.github.com>
2026-05-01 18:03:56 +0000

    series changes are added

--- series.py
+++ series.py
@@ -7,3 +7,22 @@ print(s['a':'f'])
 print(s.iloc[8]) # index based
 print(s.iloc[[1,4,6]])
 print(s.loc['f']) # labled bsed
+# ---------------------------------
+import pandas as pd
+cars = {
+    "Tata Punch": 1000000,
+    "Maruti Swift": 800000,
+    "Hyundai i20": 900000,
+    "Tata Nexon": 1200000,
+    "Mahindra Thar": 1500000,
+    "Kia Seltos": 1400000,
+    "Toyota Fortuner": 3500000,
+    "Honda City": 1300000,
+    "Skoda Slavia": 1200000,
+    "Volkswagen Virtus": 1300000
+}
+s= pd.Series(cars, name = 'cars')
+# print(s>1000000)
+print(s[s>=1000000])
+print(s[(s>=1000000) & (s<=1500000)])
+print(s[~(s>=1000000)])
